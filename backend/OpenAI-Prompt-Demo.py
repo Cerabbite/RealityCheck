6750b5b1a5856f91c4d564712ee4b7868f7bcf66
jasperdemmers <jasper.demmers@hotmail.com>
2024-02-21 12:13:38 +0100

    Changed Demo back to demo-persona.json.

--- backend/OpenAI-Prompt-Demo.py
+++ backend/OpenAI-Prompt-Demo.py
@@ -1,40 +1,38 @@
-import json  # noqa: D100
-import os
-
-from dotenv import load_dotenv
 from openai import OpenAI
-from betaface import get_facial_features
+from dotenv import load_dotenv
+import os
+import json
 
 load_dotenv()
 
-# Demo pic: https://www.faceplusplus.com/demo/images/demo-pic35.jpg
-return_code, data = get_facial_features(filename="demo-pic35.jpg", url="https://www.faceplusplus.com/demo/images/demo-pic35.jpg")
-if return_code != 0:
-    raise Exception(f"Received a non zero return code: {return_code}\n{data}")
+# DEMO ONLY DOES DEMO DATA
+with open('demo-persona.json', "r") as file:
+    data = json.load(file)
 
 tags = data["tags"]
-output_string = ", ".join(
-    f'{tag["name"]}: {tag["value"]} ({tag["confidence"]*100:.0f}%)' for tag in tags
-)
+output_string = ', '.join(f'{tag["name"]}: {tag["value"]} ({tag["confidence"]*100:.0f}%)' for tag in tags)
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
 )
 
 response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "You will only be given tags about someones face with a yes or no value. Based on these values, give 5 compliments and 5 roasts, format these in 2 lists, not markdown. Do not add extra text. The roasts must be mean and the compliments must be nice.",
-        },
-        {"role": "user", "content": output_string},
-    ],
-    temperature=1,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "system",
+      "content": "You will only be given tags about someones face with a yes or no value. Based on these values, give 5 compliments and 5 roasts, format these in 2 lists, not markdown. Do not add extra text. The roasts must be mean and the compliments must be nice."
+    },
+    {
+      "role": "user",
+      "content": output_string
+    }
+  ],
+  temperature=1,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
 )
 
 response_content = response.choices[0].message.content
